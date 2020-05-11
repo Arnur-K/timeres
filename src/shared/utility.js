c83161bab586ba50e5fbe44dedcd474c7a435ed4
Arnur Kupanov <ncase01@gmail.com>
2020-05-11 21:46:59 +0600

    localization removed

--- src/shared/utility.js
+++ src/shared/utility.js
@@ -1,11 +1,3 @@
-import store from '../store/store';
-
-let lang = null;
-
-store.subscribe(() => {
-  lang = store.getState().ui.lang;
-});
-
 export const updateObject = (oldObject, updatedProperties) => ({
   ...oldObject,
   ...updatedProperties,
@@ -19,10 +11,8 @@ export const checkValidity = (value, rules) => {
   if (rules.required) {
     valid.value = value.trim() !== '' && valid.value;
 
-    if (!valid.value && lang === 'en') {
+    if (!valid.value) {
       valid.message = ' is required.';
-    } else if (!valid.value && lang === 'ru') {
-      valid.message = ', требуется';
     } else {
       valid.message = '';
     }
@@ -31,29 +21,19 @@ export const checkValidity = (value, rules) => {
   if (rules.minLength) {
     valid.value = value.length >= rules.minLength && valid.value;
 
-    if (!valid.value && lang === 'en') {
+    if (!valid.value) {
       valid.message = ` should be at least ${rules.minLength} charachters.`;
-    } else if (!valid.value && lang === 'ru') {
-      valid.message = `, требуемое количество симвоов ${rules.minLength}.`;
     } else {
       valid.message = '';
     }
-
-    /* valid.message = !valid.value
-      ? lang === 'en'
-        ? ` should be at least ${rules.minLength} charachters.`
-        : `, требуемое количество симвоов ${rules.minLength}.`
-      : null; */
   }
 
   if (rules.isEmail) {
     const pattern = /^([a-zA-Z0-9_\-.]+)@([a-zA-Z0-9_\-.]+)\.([a-zA-Z]{2,5})$/;
     valid.value = pattern.test(value) && valid.value;
 
-    if (!valid.value && lang === 'en') {
+    if (!valid.value) {
       valid.message = ' is invalid.';
-    } else if (!valid.value && lang === 'ru') {
-      valid.message = ' недействительный.';
     } else {
       valid.message = '';
     }
